20f74229b1fd6c3dca9ac3596bda59c6d7fdebee
sergius <pechenin.sergej@gmail.com>
2020-02-26 22:18:07 +0300

    Test

--- main.cpp
+++ main.cpp
@@ -1,7 +1,16 @@
 #include <iostream>
 #include <gtest.h>
+#include <sys/time.h>
 #include "reactive-framework-cpp.h"
 using namespace std;
+
+TEST(gettimeofdayTest, base) {
+  struct timeval tv;
+  gettimeofday(&tv, NULL);
+  cout << "tv.tv_sec = " << tv.tv_sec << endl;
+  cout << "tv.tv_usec = " << tv.tv_usec << endl;
+  ASSERT_GT(tv.tv_sec, 1582744264);
+}
 
 int main(int argc, char **argv) {
   testing::InitGoogleTest(&argc, argv);
--- reactive-framework-cpp.h
+++ reactive-framework-cpp.h
@@ -1,6 +1,24 @@
 #ifndef REACTIVE_FRAMEWORK_CPP_H_
 #define REACTIVE_FRAMEWORK_CPP_H_
-namespace reacf{
+/**
+ * Цель проекта: создание системы реактивных объектов.
+ *
+ * Примерный способ применения:
+ *  reacf::Property a(0), b(0), result(0);
+ *  result = a + b; // result == 0;
+ *  a = 10; // result == 10;
+ *  b = 20; // result == 30;
+ *
+ * Объекты системы должны прозрачно передаваться
+ * через разные каналы(например ethernet)
+ */
+namespace reacf {
+class Event {
+//  T value_;
+};
 
-} // namespace reacf
+class Stream{
+
+};
+}  // namespace reacf
 #endif /* REACTIVE_FRAMEWORK_CPP_H_ */
